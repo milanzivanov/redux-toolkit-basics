738eb694caabf3510ece13ba22df2db9ad8eba9a
milan <milanzivanov@users.noreply.github.com>
2025-03-06 17:18:31 +0100

    Providing the store and using redux data in react component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { Provider } from "react-redux";
+import { createRoot } from "react-dom/client";
+import store from "./store/index.js";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+    ,
+  </Provider>
+);
